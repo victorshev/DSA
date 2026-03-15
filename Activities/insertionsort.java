f4cedbcaf43585c3d26d962cf58eb5c63287f1cb
Victor Shevchenko <victorshevche@gmail.com>
2026-03-15 16:25:20 -0400

    Insertion Sort Practice

---
+++ Activities/insertionsort.java
@@ -0,0 +1,41 @@
+package Activities;
+import java.util.Random;
+
+public class insertionsort {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] numbers = new int[10]; 
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(100); 
+        }
+
+        System.out.println("Unsorted array:");
+        printArray(numbers);
+
+        insertionSort(numbers);
+
+        System.out.println("Sorted array:");
+        printArray(numbers);
+    }
+
+    private static void insertionSort(int[] inputArray) {
+        for (int i = 1; i < inputArray.length; i++) {
+            int currentValue = inputArray[i];
+
+            int j = i - 1;
+            while(j >= 0 && inputArray[j] > currentValue) {
+                inputArray[j + 1] = inputArray[j];
+                j--;
+            }
+            inputArray[j + 1] = currentValue;
+        }
+    }
+
+    private static void printArray(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+
+}
